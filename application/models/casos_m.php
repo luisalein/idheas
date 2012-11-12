43ea88ae39efaeab70659910f0dfbd029ce56951
macsin <marynarci@gmail.com>
2012-11-12 17:15:55 -0600

    cambios

--- application/models/casos_m.php
+++ application/models/casos_m.php
@@ -1263,8 +1263,7 @@ class Casos_m extends CI_Model {
 	  * $actoId [INT]
 	  * */
 	public function mTraerVictimasActo($actoId){
-		
-		$this->db->select('victimaId,actorId,estatusVictimaId,comentarios');
+		$this->db->select('actorId,victimaId');
 		$this->db->from('victimas');
 		$this->db->where('actos_actoId', $actoId);
 		$consultaVictimas = $this->db->get();
