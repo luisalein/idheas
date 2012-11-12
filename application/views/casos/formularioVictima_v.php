f164068575d69d77b7d89c69cb5528a85e0d91ee
luisalein <luisa.207310034@gmail.com>
2012-11-12 17:11:41 -0600

    cambios2

--- application/views/casos/formularioVictima_v.php
+++ application/views/casos/formularioVictima_v.php
@@ -6,6 +6,7 @@
 		
 	<body>
 		<pre><?= print_r($catalogos['estatusVictimaCatalogo'])?></pre>
+		<pre><?= print_r($victimas)?></pre>
 		<div class="twelve columns">
 			<div class="four columns"> 	<!--Lista de victimas-->
 				<div class="panel">
