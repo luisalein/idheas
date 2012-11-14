e8cacaf0946e775838e1afd97d725ffd49bc27fd
luisalein <luisa.207310034@gmail.com>
2012-11-14 15:49:14 -0600

    Corrección vistas transmigrante

--- application/views/actores/colectivo_v.php
+++ application/views/actores/colectivo_v.php
@@ -34,35 +34,6 @@
         <?php echo br(3);?>
         
         <fieldset> <!--Dirección-->
-              <legend>Dirección</legend>
-            <div class="six columns">
-             
-                <label><b>Ubicación:   </b> </label> 
-                <span id="direccion"><?=(isset($datosActor['direccionActor']['direccion'])) ? $datosActor['direccionActor']['direccion'] : ''; ?></span>
-             
-                <label><b>País:  </b> </label> 
-                <span id="paisesCatalogo_paisId"><?=(isset($datosActor['direccionActor']['paisesCatalogo_paisId'])) ?  $catalogos['paisesCatalogo'][$datosActor['direccionActor']['paisesCatalogo_paisId']]['nombre'] : ''; ?></span>
-             
-                <label><b>Estado:  </b> </label> 
-                <span id="estadosCatalogo_estadoId"><?=(isset($datosActor['direccionActor']['estadosCatalogo_estadoId'])) ? $catalogos['estadosCatalogo'][$datosActor['direccionActor']['estadosCatalogo_estadoId']]['nombre'] : ''; ?></span>
-             
-            </div> 
-            
-            
-            <div class="six columns">
-                
-                <label><b>Municipio: </b> </label>
-                <span id="municipiosCatalogo_municipioId"><?=(isset($datosActor['direccionActor']['municipiosCatalogo_municipioId'])) ? $catalogos['municipiosCatalogo'][$datosActor['direccionActor']['municipiosCatalogo_municipioId']]['nombre'] : ''; ?></span>
-             
-                <label><b>Código postal:  </b> </label>
-                <span id="actores_codigoPostal"><?=(isset($datosActor['actores']['codigoPostal'])) ? $datosActor['actores']['codigoPostal'] : ''; ?></span>
-
-            </div>
-        </fieldset> <!--Termina información general-->
-        <br/><br/><br/>
-        
-        
-        <fieldset> <!--Dirección-->
               <legend>Información de contacto</legend>
             <div class="six columns">
              
@@ -89,11 +60,36 @@
         </fieldset> <!--Termina información general-->
 
 
+        <br/><br/><br/>
 
+        <fieldset> <!--Dirección-->
+              <legend>Dirección</legend>
+            <div class="six columns">
+             
+                <label><b>Ubicación:   </b> </label> 
+                <span id="direccion"><?=(isset($datosActor['direccionActor']['direccion'])) ? $datosActor['direccionActor']['direccion'] : ''; ?></span>
+             
+                <label><b>País:  </b> </label> 
+                <span id="paisesCatalogo_paisId"><?=(isset($datosActor['direccionActor']['paisesCatalogo_paisId'])) ?  $catalogos['paisesCatalogo'][$datosActor['direccionActor']['paisesCatalogo_paisId']]['nombre'] : ''; ?></span>
+             
+                <label><b>Estado:  </b> </label> 
+                <span id="estadosCatalogo_estadoId"><?=(isset($datosActor['direccionActor']['estadosCatalogo_estadoId'])) ? $catalogos['estadosCatalogo'][$datosActor['direccionActor']['estadosCatalogo_estadoId']]['nombre'] : ''; ?></span>
+             
+            </div> 
+            
+            
+            <div class="six columns">
+                
+                <label><b>Municipio: </b> </label>
+                <span id="municipiosCatalogo_municipioId"><?=(isset($datosActor['direccionActor']['municipiosCatalogo_municipioId'])) ? $catalogos['municipiosCatalogo'][$datosActor['direccionActor']['municipiosCatalogo_municipioId']]['nombre'] : ''; ?></span>
+             
+                <label><b>Código postal:  </b> </label>
+                <span id="actores_codigoPostal"><?=(isset($datosActor['actores']['codigoPostal'])) ? $datosActor['actores']['codigoPostal'] : ''; ?></span>
 
+            </div>
+        </fieldset> <!--Termina información general-->
 
-
-
+        <br/><br/><br/>
 
 
 
