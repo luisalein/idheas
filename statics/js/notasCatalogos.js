36f8c3990d06bf36acfeb49d1c73e1ef5708117c
luisalein <luisa.207310034@gmail.com>
2012-11-19 16:44:22 -0600

    Relaciones

--- statics/js/notasCatalogos.js
+++ statics/js/notasCatalogos.js
@@ -1,7 +1,11 @@
-function notasCatalogos(notas,escribeNotas){
+function notasCatalogos(notas,escribeNotas,id){
 	if(notas!=""){
-		 boton='<input type="button" value="i" onclick="botonNotas("'+notas+'")" class="tiny button"/>';
-		$('#'+escribeNotas).html(boton); 
+		 boton='<input type="button" value="i" onclick="botonNotas('+notas+')" class="tiny button"/>';
+		 if(id!=""){
+		 	$('#'+escribeNotas).html(notas); 
+		 } else{
+		 	$('#'+escribeNotas).html(boton); 
+		 }
 	}
 }
 
