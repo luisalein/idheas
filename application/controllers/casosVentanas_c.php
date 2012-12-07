36ba185c38d1d3a89b33367a7eaa3247cdc53f16
macsin <marynarci@gmail.com>
2012-12-07 16:55:03 -0600

    cambios

--- application/controllers/casosVentanas_c.php
+++ application/controllers/casosVentanas_c.php
@@ -328,7 +328,6 @@ class CasosVentanas_c extends CI_Controller {
 			case(4):  
 				if($_POST['editar'] == 1){
 					$mensaje =  $this->casos_m->mActualizaDatosIntervencion($datos['intervenciones'],$datos['intervenciones']['intervencionNId']);
-
 					$mensaje = $mensaje . $this->casos_m->mActualizaDatosIntervenido($datos['intervenidos'],$datos['intervenidos']['intervenidoId']);
 				}else{
 					$datos4['intervencion'] = $datos['intervenciones'];
