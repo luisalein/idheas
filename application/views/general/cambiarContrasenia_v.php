943fe8b05763a9aa620d377328ae1b6125706fa8
macsin <marynarci@gmail.com>
2012-12-10 13:34:48 -0600

    agregar eliminar intervenido

--- application/views/general/cambiarContrasenia_v.php
+++ application/views/general/cambiarContrasenia_v.php
@@ -1,11 +1,16 @@
 <form method="post"  action='' accept-charset="utf-8">
-	<fieldset class="six columns">
-	<div class="six columns">
+	<fieldset style="padding: 15px;" class="four columns">
+	<div class="twelve columns">
 		<label for="nombre">Contraseña Actual</label>
-		<input type="text" id="nombreCaso" name="nombreCaso"  required />
-		<input type="hidden" id="tipoFiltro" name="tipoFiltro" value="1"/>
+		<input type="text" id="oldPass" name="oldPass"  required />
+		<label for="nombre">Nueva Contraseña</label>
+		<input type="text" id="newPass1" name="newPass1"  required />
+		<label for="nombre">Repetir Nueva Contraseña</label>
+		<input type="text" id="newPass2" name="newPass2"  required />
 		<br />
-		<input class="medium button" type="submit" value="Guardar" />
+		
+		<input style="float:right;" class="medium button" type="button" value="Cancelar" />
+		<input style="float:right; margin-right: 10px;" class="medium button" type="button" value="Guardar" />
 	</div>	
 	
 	
