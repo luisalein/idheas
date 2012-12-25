6e2417b4f8b38338b33c267133c2ae0d24d0da66
macsin <marynarci@gmail.com>
2012-12-25 12:39:56 -0600

    cambios2

--- statics/js/reporte.js
+++ statics/js/reporte.js
@@ -1,9 +1,10 @@
 /***Script para que funcione el date picker****/
 $(function() {
-	$( "#desdeFechaReporte" ).datepicker({ dateFormat: "yy-mm-dd"});
-	 });
+	$( "#desdeFechaReporte" ).datepicker({ dateFormat: "yy-mm-dd",
+		changeYear: true});
+		 });
 	 
 $(function() {
-	$( "#hastaFechaReporte" ).datepicker({ dateFormat: "yy-mm-dd"});
-	 }	 );
-		 
+	$( "#hastaFechaReporte" ).datepicker({ dateFormat: "yy-mm-dd",
+		changeYear: true});
+		 });
