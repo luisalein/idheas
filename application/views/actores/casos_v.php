e058b3cfcaa576aecd4b55e1aaeb26499b3a6382
luisalein <luisa.207310034@gmail.com>
2012-12-31 01:40:19 -0600

    Arreglo comentarios

--- application/views/actores/casos_v.php
+++ application/views/actores/casos_v.php
@@ -1,6 +1,7 @@
 <div id="pestania" data-collapse>	
 	<h2 class="open">Casos</h2><!--título de la pestaña-->  
 	<div>
+		<pre><?= print_r($casosRelacionados)?></pre>
 	<?php if (isset($casosRelacionados) && ($casosRelacionados!=0)) {?>
 	<?php foreach ($casosRelacionados as $datosCaso) {?>
 		<div class="twelve columns" data-collapse>	
