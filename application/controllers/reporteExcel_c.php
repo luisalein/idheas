361ca9407263ed9bfe63b0d37a40ada66c907178
brass3a4 <brass3a4@gmail.com>
2013-01-15 19:19:03 -0600

    reporte corto

--- application/controllers/reporteExcel_c.php
+++ application/controllers/reporteExcel_c.php
@@ -280,16 +280,16 @@ class ReporteExcel_c extends CI_Controller
 			
 		}
 		
-		// $filename='reporte_corto.xls'; //save our workbook as this file name
-		// header('Content-Type: application/vnd.ms-excel'); //mime type
-		// header('Content-Disposition: attachment;filename="'.$filename.'"'); //tell browser what's the file name
-		// header('Cache-Control: max-age=0'); //no cache
-// 					             
-		// //save it to Excel5 format (excel 2003 .XLS file), change this to 'Excel2007' (and adjust the filename extension, also the header mime type)
-		// //if you want to save it as .XLSX Excel 2007 format
-		// $objWriter = PHPExcel_IOFactory::createWriter($this->excel, 'Excel5');  
-		// //force user to download the Excel file without writing it to server's HD
-		// $objWriter->save('php://output');
+		$filename='reporte_corto.xls'; //save our workbook as this file name
+		header('Content-Type: application/vnd.ms-excel'); //mime type
+		header('Content-Disposition: attachment;filename="'.$filename.'"'); //tell browser what's the file name
+		header('Cache-Control: max-age=0'); //no cache
+					             
+		//save it to Excel5 format (excel 2003 .XLS file), change this to 'Excel2007' (and adjust the filename extension, also the header mime type)
+		//if you want to save it as .XLSX Excel 2007 format
+		$objWriter = PHPExcel_IOFactory::createWriter($this->excel, 'Excel5');  
+		//force user to download the Excel file without writing it to server's HD
+		$objWriter->save('php://output');
 	}
 
 }
